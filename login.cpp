fef13486edf7d5bce88de2cb9dcdfcbbea99422f
AFP <alexandre.francois.pena@tutanota.com>
2024-05-17 11:07:08 +0200

    added Function to login a  user and Open file to read user data

--- login.cpp
+++ login.cpp
@@ -22,3 +22,28 @@ void registerUser() {
     }
 }
 
+// Function to login a user
+bool loginUser() {
+    std::string username, password;
+    std::cout << "Enter your username: ";
+    std::cin >> username;
+    std::cout << "Enter your password: ";
+    std::cin >> password;
+
+    // Open file to read user data
+    std::ifstream file("users.txt");
+    std::string usr, pwd;
+    if (file.is_open()) {
+        while (file >> usr >> pwd) {
+            if (usr == username && pwd == password) {
+                std::cout << "Login successful.\n";
+                return true;
+            }
+        }
+        file.close();
+    } else {
+        std::cout << "Failed to open file for login.\n";
+    }
+    std::cout << "Login failed.\n";
+    return false;
+}
